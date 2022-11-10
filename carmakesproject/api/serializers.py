ad89f20942299e7485ffa36d5cd4572855875655
sowinskipatryk <sowinski.patryk@wp.pl>
2022-11-10 09:30:21 +0100

    added car makes and models POST validation

--- carmakesproject/api/serializers.py
+++ carmakesproject/api/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from carmakesapp.models import Car, Rate
 
+
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ('id', 'make', 'model', 'avg_rating')
 
+
 class PopularSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ('id', 'make', 'model', 'rates_number')
+
 
 class RateSerializer(serializers.ModelSerializer):
     class Meta:
--- carmakesproject/api/views.py
+++ carmakesproject/api/views.py
@@ -3,23 +3,50 @@ from rest_framework.decorators import api_view
 from carmakesapp.models import Car, Rate
 from .serializers import CarSerializer, RateSerializer, PopularSerializer
 from django.core.exceptions import ObjectDoesNotExist
+import requests
+
 
 @api_view(['GET', 'POST'])
 def get_post_cars(request):
     if request.method == 'POST':
         serializer = CarSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            post_make = request.data['make']
+            post_model = request.data['model']
+            try:
+                makes = requests.get('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json')
+                models = requests.get(f'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{post_make}?format=json')
+
+                make_found = False
+                model_found = False
+                for make in makes.json()['Results']:
+                    if make['Make_Name'] == post_make.upper():
+                        make_found = True
+                        break
+
+                for model in models.json()['Results']:
+                    if model['Model_Name'] == post_model:
+                        model_found = True
+                        break
+
+                if make_found and model_found:
+                    serializer.save()
+                    return Response(serializer.data)
+                else:
+                    return Response('The car with given make and model does not exist in the validation database!',
+                                    status=400)
+            except requests.exceptions.InvalidSchema:
+                return Response('Validation database failed!', status=500)
         return Response('Wrong data format! Car should have the make and model.', status=400)
     cars = Car.objects.all()
     serializer = CarSerializer(cars, many=True)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
 def add_rate(request):
     serializer = RateSerializer(data=request.data)
-    if not request.data['rating'] in range(1,6):
+    if not request.data['rating'] in range(1, 6):
         return Response('Rating must be an integer from 1 to 5!', status=400)
     if serializer.is_valid():
         serializer.save()
@@ -29,6 +56,7 @@ def add_rate(request):
         return Response(serializer.data)
     return Response('The car with given ID does not exist in the database!', status=400)
 
+
 @api_view(['DELETE'])
 def delete_car(request, pk):
     try:
@@ -37,6 +65,7 @@ def delete_car(request, pk):
         return Response('Car deleted successfully!')
     except ObjectDoesNotExist:
         return Response('The car with given ID does not exist in the database!', status=400)
+
 
 @api_view(['GET'])
 def get_popular(request):
