395bb11e72a29b2d2cd62a24ea435b6bcc5b3fef
sowinskipatryk <sowinski.patryk@wp.pl>
2022-11-11 23:49:11 +0100

    docker configuration

--- carmakesproject/carmakesproject/settings.py
+++ carmakesproject/carmakesproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8n#h*s67_u)2)iwo2hepc%2w@gng)ic)07*6x&582$jb!*-x-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
